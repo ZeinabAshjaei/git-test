f0a58cf6d2e1ccb6a94dbabe3d8924463e59a589
zeinab <z.ashjaei@gmail.com>
2017-11-11 14:45:04 +0200

    get one user feature based on id is added
    This feature is an API which takes the id, searches
    for the user with the same id, and returns 200 satatus code and
    the user if available, if not, return 404 status code.

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ app.get('/users', function(req,res){
   res.send(JSON.stringify(users));
 });
 
+app.get('/users/:id', function(req,res){
+  var id = req.params.id;
+  var user = users.filter(function(user){
+    return user.id == id;
+  });
+  if(user.length){
+    res.status(200).send(JSON.stringify(user[0]));
+  } else {
+    res.status(404).send(new Error('the user with id ' + id + ' is not found').toString());
+  }
+});
+
 app.listen(8080, function(){
   console.log('app is listening on port 8080!');
 });
